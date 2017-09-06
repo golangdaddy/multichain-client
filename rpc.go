85e182ce3d26c012100f26816960b6ae6c5ef998
abreadman <cryptoconference@gmail.com>
2017-09-06 17:32:56 +0100

    fix account

--- rpc.go
+++ rpc.go
@@ -97,6 +97,23 @@ func (client *Client) GetNewAddress() (Response, error) {
 	return obj, nil
 }
 
+func (client *Client) GetAddressBalances(address string) (Response, error) {
+
+	msg := map[string]interface{}{
+		"jsonrpc": "1.0",
+		"id": CONST_ID,
+		"method": "getnewaddress",
+		"params": []interface{}{address},
+	}
+
+	obj, err := client.post(msg)
+	if err != nil {
+		return nil, err
+	}
+
+	return obj, nil
+}
+
 func (client *Client) CreateKeypair() (*AddressKeyPair, error) {
 
 	msg := map[string]interface{}{
