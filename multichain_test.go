3ec11f521447d26186f8576a8861fb67babc8ab5
abreadman <cryptoconference@gmail.com>
2017-09-19 17:38:30 +0100

    add chain name

--- multichain_test.go
+++ multichain_test.go
@@ -9,14 +9,16 @@ var client *Client
 
 func init() {
 
-	host := flag.String("host", "localhost", "is a string")
-	port := flag.String("port", "80", "is a string")
-	username := flag.String("username", "multichainrpc", "is a string")
-	password := flag.String("password", "12345678", "is a string")
+	chain := flag.String("chain", "", "is the name of the chain")
+	host := flag.String("host", "localhost", "is a string for the hostname")
+	port := flag.String("port", "80", "is a string for the host port")
+	username := flag.String("username", "multichainrpc", "is a string for the username")
+	password := flag.String("password", "12345678", "is a string for the password")
 
 	flag.Parse()
 
 	client = NewClient(
+		*chain,
 		*host,
 		*port,
 		*username,
