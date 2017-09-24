650c42a64143fb4b494280b5b35767b4d7f192fc
abreadman <cryptoconference@gmail.com>
2017-09-24 22:34:28 +0100

    add methods

---
+++ api_createrawsendfrom.go
@@ -0,0 +1,24 @@
+package multichain
+
+// This works like createrawtransaction, except it automatically selects the transaction inputs from those belonging to from-address, to cover the appropriate amounts. One or more change outputs going back to from-address will also be added to the end of the transaction.
+func (client *Client) CreateRawSendFrom(watchAddress, destinationAddress string, assets map[string]float64) (Response, error) {
+
+	msg := map[string]interface{}{
+		"jsonrpc": "1.0",
+		"id": CONST_ID,
+		"method": "createrawsendfrom",
+		"params": []interface{}{
+			watchAddress,
+			map[string]interface{}{
+				destinationAddress: assets,
+			},
+		},
+	}
+
+	obj, err := client.post(msg)
+	if err != nil {
+		return nil, err
+	}
+
+	return obj, nil
+}
