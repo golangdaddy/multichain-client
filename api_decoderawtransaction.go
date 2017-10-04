019a3b73cf8cfa81c13c05c6a979dd4c6c050284
abreadman <cryptoconference@gmail.com>
2017-10-04 15:34:46 +0100

    add decoderawtransacttion method

--- api_decoderawtransaction.go
+++ api_decoderawtransaction.go
@@ -1,13 +1,15 @@
 package multichain
 
 // Adds address (or a full public key, or an array of either) to the wallet, without an associated private key. This creates one or more watch-only addresses, whose activity and balance can be retrieved via various APIs (e.g. with the includeWatchOnly parameter), but whose funds cannot be spent by this node. If rescan is true, the entire blockchain is checked for transactions relating to all addresses in the wallet, including the added ones. Returns null if successful.
-func (client *Client) ImportAddress(pubKey, label string, rescan bool) (Response, error) {
+func (client *Client) DecodeRawTransaction(rawTransaction string) (Response, error) {
 
 	msg := map[string]interface{}{
 		"jsonrpc": "1.0",
 		"id": CONST_ID,
-		"method": "importaddress",
-		"params": []interface{}{pubKey, label, rescan},
+		"method": "decoderawtransaction",
+		"params": []interface{}{
+			rawTransaction,
+		},
 	}
 
 	obj, err := client.post(msg)
