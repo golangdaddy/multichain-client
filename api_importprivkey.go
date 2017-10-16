646dfb1114f5b04fa3f539bff03531020a84ccfd
abreadman <cryptoconference@gmail.com>
2017-10-16 20:45:53 +0100

    fix importprivkey

--- api_importprivkey.go
+++ api_importprivkey.go
@@ -1,7 +1,5 @@
 package multichain
 
-import "fmt"
-
 //Adds a privkey private key (or an array thereof) to the wallet, together with its associated public address. If rescan is true, the entire blockchain is checked for transactions relating to all addresses in the wallet, including the added ones. Returns null if successful.
 func (client *Client) ImportPrivKey(privKey, label string, rescan bool) (Response, error) {
 
@@ -12,7 +10,7 @@ func (client *Client) ImportPrivKey(privKey, label string, rescan bool) (Respons
 		"params": []interface{}{
 			[]string{privKey},
 			label,
-			fmt.Sprintf("rescan=%v", rescan),
+			rescan,
 		},
 	}
 
