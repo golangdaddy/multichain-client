d0fb066106fe3565619f450a6e194c49971fb754
Apple <apple@apples-mbp.home>
2017-10-22 17:15:27 +0100

    add signrawtransaction method

---
+++ api_signrawtransaction.go
@@ -0,0 +1,20 @@
+package multichain
+
+func (client *Client) SignRawTransaction(rawTransaction string) (Response, error) {
+
+	msg := map[string]interface{}{
+		"jsonrpc": "1.0",
+		"id": CONST_ID,
+		"method": "signrawtransaction",
+		"params": []interface{}{
+			rawTransaction,
+		},
+	}
+
+	obj, err := client.post(msg)
+	if err != nil {
+		return nil, err
+	}
+
+	return obj, nil
+}
