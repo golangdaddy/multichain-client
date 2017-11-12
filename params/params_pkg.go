3c8ecd815111d3a8bf5cc669ac7610cc5b37ed2c
A <alex@cpu.host>
2017-11-12 19:47:57 +0000

    add params pkg

--- params/params_pkg.go
+++ params/params_pkg.go
@@ -2,6 +2,7 @@ package params
 
 import (
     "strings"
+    "strconv"
     "io/ioutil"
 )
 
@@ -36,4 +37,44 @@ func Open(pathToFile string) (Params, error) {
     }
 
     return params, nil
+}
+
+// Params methods
+
+func (params Params) Bool(key string) bool {
+
+    value := params[key]
+
+    switch value {
+
+        case "true":
+
+            return true
+
+        case "false":
+
+            return false
+
+    }
+
+    panic("Invalid BOOL value for key: "+key)
+
+    return false
+}
+
+func (params Params) Int(key string) int {
+
+    value := params[key]
+
+    i, err := strconv.Atoi(value)
+    if err != nil {
+        panic(err)
+    }
+
+    return i
+}
+
+func (params Params) String(key string) string {
+
+    return params[key]
 }
