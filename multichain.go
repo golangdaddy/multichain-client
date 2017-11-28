80f1e60122aa80cc2392c0280b795fd531eb1a75
A <alex@cpu.host>
2017-11-28 21:10:16 +0000

    added support for fallback hosts

--- multichain.go
+++ multichain.go
@@ -3,6 +3,7 @@ package multichain
 import (
 	"fmt"
 	"errors"
+	"strings"
 	"net/http"
 	"io/ioutil"
 	"encoding/json"
@@ -25,7 +26,7 @@ type Client struct {
 	chain string
 	httpClient *http.Client
 	port string
-	endpoint string
+	endpoints []string
 	credentials string
 }
 
@@ -37,59 +38,77 @@ func NewClient(chain, host, port, username, password string) *Client {
 		chain: chain,
 		httpClient: &http.Client{},
 		port: port,
-		endpoint: fmt.Sprintf("http://%s:%s", host, port),
+		endpoints: []string{fmt.Sprintf("http://%s:%s", host, port)},
 		credentials: base64.StdEncoding.EncodeToString([]byte(credentials)),
 	}
 }
 
 // Creates a new temporary config for calling an RPC method on the specified node
-func (client *Client) ViaNode(host string) *Client {
+func (client *Client) ViaNodes(hosts []int) *Client {
 
 	c := *client
 
-	c.endpoint = fmt.Sprintf("http://%s:%s", host, client.port)
+	for _, host := range hosts {
+
+		c.endpoints = append(c.endpoints, fmt.Sprintf("http://%s:%s", host, client.port))
+
+	}
 
 	return &c
 }
 
 func (client *Client) debug() string {
-	return client.endpoint + " " + client.credentials
+	return strings.Join(client.endpoints, " ") + " " + client.credentials
 }
 
 func (client *Client) post(msg interface{}) (Response, error) {
 
-	request, err := sling.New().Post(client.endpoint).BodyJSON(msg).Request()
+	for i, endpoint := range client.endpoints {
 
-	request.Header.Add("Authorization", "Basic " + client.credentials)
+		request, err := sling.New().Post(endpoint).BodyJSON(msg).Request()
 
-	resp, err := client.httpClient.Do(request)
-	if err != nil {
-		return nil, err
-	}
+		request.Header.Add("Authorization", "Basic " + client.credentials)
 
-	b, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return nil, err
-	}
-
-	obj := make(Response)
-
-	if err := json.Unmarshal(b, &obj); err != nil {
-		fmt.Println(string(b))
-		return nil, err
-	}
-
-	if obj["error"] != nil {
-		e := obj["error"].(map[string]interface{})
-		var s string
-		m, ok := msg.(map[string]interface{})
-		if ok {
-			s = fmt.Sprintf("multichaind/%s: %s", m["method"], e["message"].(string))
-		} else {
-			s = fmt.Sprintf("multichaind: %s", e["message"].(string))
+		resp, err := client.httpClient.Do(request)
+		if err != nil {
+			if (i + 1) == len(client.endpoints) {
+				return nil, err
+			}
+			continue
 		}
-		return nil, errors.New(s)
+
+		b, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			if (i + 1) == len(client.endpoints) {
+				return nil, err
+			}
+			continue
+		}
+
+		obj := make(Response)
+
+		err = json.Unmarshal(b, &obj)
+		if err != nil {
+			if (i + 1) == len(client.endpoints) {
+				return nil, err
+			}
+			continue
+		}
+
+		if obj["error"] != nil {
+			e := obj["error"].(map[string]interface{})
+			var s string
+			m, ok := msg.(map[string]interface{})
+			if ok {
+				s = fmt.Sprintf("multichaind/%s: %s", m["method"], e["message"].(string))
+			} else {
+				s = fmt.Sprintf("multichaind: %s", e["message"].(string))
+			}
+			return nil, errors.New(s)
+		}
+
+		return obj, nil
 	}
 
-	return obj, nil
+	return nil, errors.New("PROBABLY NO ENDPOINTS PASSED TO THE REQUEST DISPATCHER")
 }
