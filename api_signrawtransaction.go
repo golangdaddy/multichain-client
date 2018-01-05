3ab4b1d08cd12092f72050e4c431f7b5e80eec29
A <alex@cpu.host>
2018-01-05 16:35:05 +0000

    allow additional args to signrawtransaction

--- api_signrawtransaction.go
+++ api_signrawtransaction.go
@@ -2,13 +2,17 @@ package multichain
 
 func (client *Client) SignRawTransaction(rawTransaction string, txDataArray []*TxData, privateKey string, args ...string) (Response, error) {
 
+	if txDataArray == nil {
+		txDataArray = []*TxData{}
+	}
+
 	params := []interface{}{
 		rawTransaction,
 		txDataArray,
 		[]string{privateKey},
 	}
 	for _, arg := range args {
-		params = append(params, arg)		
+		params = append(params, arg)
 	}
 
 	msg := client.NodeMsg(
