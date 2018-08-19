8ff08577e66dc529445b17f7cf34746794e491af
Rafael Medeiros <98@99.exchange>
2018-08-19 15:26:36 -0300

    remove fun bitcoinaddress because repo dont exists anymore `github.com/utamaro/gocoin`

--- address/addresses.go
+++ address/addresses.go
@@ -1,45 +1,30 @@
 package address
 
 import (
-//    "fmt"
-    "github.com/utamaro/gocoin"
-    "github.com/mr-tron/base58/base58"
+	//    "fmt"
+	"github.com/mr-tron/base58/base58"
 )
-
-func BitcoinAddress(input []byte) (string, error) {
-
-    if !configued { panic(CONST_UNCONFIGURED) }
-
-    privateKey := BitcoinWIF(input)
-
-    k, err := gocoin.GetKeyFromWIF(privateKey)
-    if err != nil {
-        return "", err
-    }
-
-    publicKey, _ := k.Pub.GetAddress()
-
-    return publicKey, nil
-}
 
 func MultiChainAddress(input []byte) (string, error) {
 
-    if !configued { panic(CONST_UNCONFIGURED) }
+	if !configued {
+		panic(CONST_UNCONFIGURED)
+	}
 
-    x := int(20 / len(address_pubkeyhash_version))
+	x := int(20 / len(address_pubkeyhash_version))
 
-    b := ripemd(sha(input))
+	b := ripemd(sha(input))
 
-    extended := []byte{}
-    for index := 0; index < len(address_pubkeyhash_version); index++ {
-        extended = append(extended, address_pubkeyhash_version[ index : (index + 1) ]...)
-        extended = append(extended, b[ (x * index) : (index * x) + x ]...)
-    }
+	extended := []byte{}
+	for index := 0; index < len(address_pubkeyhash_version); index++ {
+		extended = append(extended, address_pubkeyhash_version[index:(index+1)]...)
+		extended = append(extended, b[(x*index):(index*x)+x]...)
+	}
 
-    b = make([]byte, 4)
-    safeXORBytes(b, address_checksum_value, sha(sha(extended))[:4])
+	b = make([]byte, 4)
+	safeXORBytes(b, address_checksum_value, sha(sha(extended))[:4])
 
-    b = append(extended, b...)
+	b = append(extended, b...)
 
-    return string(base58.FastBase58Encoding(b)), nil
+	return string(base58.FastBase58Encoding(b)), nil
 }
