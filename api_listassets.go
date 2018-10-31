8e5e1afda2de0e342ec3a020983d0704207d7854
rafaeltokyo <rafaelgurudesign@gmail.com>
2018-10-31 19:11:07 -0300

    change description in func

--- api_listassets.go
+++ api_listassets.go
@@ -1,6 +1,6 @@
 package multichain
 
-// Returns a list of unspent transaction outputs in the wallet, with between minconf and maxconf confirmations. For a MultiChain blockchain, each transaction output includes assets and permissions fields listing any assets or permission changes encoded within that output. If the third parameter is provided, only outputs which pay an address in this array will be included.
+// Returns a list of assets in multichain
 func (client *Client) ListAssets(assets string, verbose bool, count int, start int) (Response, error) {
 
 	msg := client.Command(
