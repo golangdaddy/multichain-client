d5ca65c4ebdc962fa1570ed32ec2a1188b72acbd
Nils Kuhn <nils.kuhn@iteratec.de>
2020-06-15 13:02:44 +0200

    :bug: use method 'publish' for publishing
    additionally provide separate methods for publishing data on- or offchain

--- api_publish.go
+++ api_publish.go
@@ -1,19 +1,41 @@
 package multichain
 
-import (
-	"fmt"
-)
+// Publishes an item in a stream (passed as a stream name, ref or creation txid). Data is held inside blockchain.
+//
+// Pass a single textual key or an array thereof in the key(s) parameter.
+// The data parameter can accept raw hexadecimal data like a1b2c3d4, a reference to the binary cache
+// {"cache":"Ev1HQV1aUCY"}, textual data {"text":"hello world"} or JSON data {"json":{"i":[1,2],"j":"yes"}}.
+// Returns the txid of the transaction sent.
+// To easily publish multiple items in a single transaction, see the publishmulti(from) command.
+func (client *Client) PublishOnchain(stream string, keys []string, data interface{}) (Response, error) {
+	return client.publish(stream, keys, data, false)
+}
 
-//Creates a new stream on the blockchain called name. For now, always pass the value "stream" in the type parameter – this is designed for future functionality. If open is true then anyone with global send permissions can publish to the stream, otherwise publishers must be explicitly granted per-stream write permissions.
-func (client *Client) Publish(typeName, name string, open bool) (Response, error) {
+// Publishes an item in a stream (passed as a stream name, ref or creation txid). Data is held outside blockchain
+// (offchain).
+//
+// Pass a single textual key or an array thereof in the key(s) parameter.
+// The data parameter can accept raw hexadecimal data like a1b2c3d4, a reference to the binary cache
+// {"cache":"Ev1HQV1aUCY"}, textual data {"text":"hello world"} or JSON data {"json":{"i":[1,2],"j":"yes"}}.
+// Returns the txid of the transaction sent.
+// To easily publish multiple items in a single transaction, see the publishmulti(from) command.
+func (client *Client) PublishOffchain(stream string, keys []string, data interface{}) (Response, error) {
+	return client.publish(stream, keys, data, true)
+}
 
+func (client *Client) publish(stream string, keys []string, data interface{}, offchain bool) (Response, error) {
+
+	params := []interface{}{
+		stream,
+		keys,
+		data,
+	}
+	if offchain {
+		params = append(params, "offchain")
+	}
 	msg := client.Command(
-		"create",
-		[]interface{}{
-			"type="+typeName,
-			name,
-			fmt.Sprintf("open=%v", open),
-		},
+		"publish",
+		params,
 	)
 
 	return client.Post(msg)
